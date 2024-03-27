be75c74fe084108cf9e8e247ee06ac488d45103b
David <fxj5fe@virginia.edu>
2024-03-26 22:39:40 -0400

    Add M09 Participation

---
+++ demo/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/david-hernandz/M09-Exercise',
+    author='David Hernandez',
+    author_email='fxj5fe@virginia.edu',
+    license='MIT'
+    packages=['demo']
+)
